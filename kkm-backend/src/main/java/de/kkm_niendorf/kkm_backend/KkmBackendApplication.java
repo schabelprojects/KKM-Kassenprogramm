ac24808c49def529c8f69a253356df30f31ed307
simplydev24 <simplydev@gmx.de>
2025-01-30 15:47:57 +0100

    initial commit

---
+++ kkm-backend/src/main/java/de/kkm_niendorf/kkm_backend/KkmBackendApplication.java
@@ -0,0 +1,13 @@
+package de.kkm_niendorf.kkm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkmBackendApplication.class, args);
+	}
+
+}
---
+++ kkm-backend/src/test/java/de/kkm_niendorf/kkm_backend/KkmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kkm_niendorf.kkm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
